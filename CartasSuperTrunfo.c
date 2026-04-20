e672f4cb9fe9dca8897a8f2215a94a062353ef82
MOYSESDASILVASOUZA <moysesaber@gmail.com>
2026-04-20 19:35:41 +0000

    commit novato e aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,10 +6,113 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
+    
+char estado1;
+char codigo1[4];
+char cidade1[20];
+int populacao1;
+float area1;
+float pib1;
+int nturisticos1;
+
+char estado2;
+char codigo2[4];
+char cidade2[20];
+int populacao2;
+float area2;
+float pib2;
+int nturisticos2;
+
+float densidade1;
+float densidade2;
+float pibcapita1;
+float pibcapita2;
 
   // Área para entrada de dados
 
-  // Área para exibição dos dados da cidade
+printf("Dados da Carta 1: \n");
+printf("Digite a letra do Estado (Ex.: C - Ceará): \n");
+scanf(" %c", &estado1); 
 
+printf("Digite o código da carta (ex: A01, B03): \n");
+scanf("%3s", codigo1);
+    
+printf("Digite o nome da cidade: \n");
+scanf(" %[^\n]", cidade1); 
+
+printf("Digite a população: \n");
+scanf("%d", &populacao1);
+
+getchar();
+
+printf("Digite a área (km²): \n");
+scanf("%f", &area1);
+
+printf("Digite o PIB: \n");
+scanf("%f", &pib1);
+
+getchar();
+
+printf("Digite o número de pontos turísticos: \n");
+scanf("%d", &nturisticos1);
+
+printf("Dados da Carta 2: \n");
+printf("Digite a letra do Estado (Ex.: C - Ceará): \n");
+scanf(" %c", &estado2); 
+
+printf("Digite o código da carta (ex: A01, B03): \n");
+scanf("%3s", codigo2);
+    
+printf("Digite o nome da cidade: \n");
+scanf(" %[^\n]", cidade2); 
+
+printf("Digite a população: \n");
+scanf("%d", &populacao2);
+
+getchar();
+
+printf("Digite a área (km²): \n");
+scanf("%f", &area2);
+
+printf("Digite o PIB: \n");
+scanf("%f", &pib2);
+
+getchar();
+
+printf("Digite o número de pontos turísticos: \n");
+scanf("%d", &nturisticos2);
+
+  // Área para cálculos
+
+  densidade1 = populacao1 / area1;
+  densidade2 = populacao2 / area2;
+
+  pibcapita1 = pib1 / populacao1;
+  pibcapita2 = pib2 / populacao2;
+
+  // Área para exibição dos dados da cidade
+printf("\nCarta 1: \n\n");
+printf("Estado: %c \n", estado1);
+printf("Código: %s \n", codigo1);
+printf("Cidade: %s \n", cidade1);
+printf("População: %d\n", populacao1);
+printf("Área: %.2f km²\n", area1);
+printf("PIB: %.2f\n", pib1);
+printf("Pontos turísticos: %d\n", nturisticos1);
+printf("Densidade populacional: %.2f\n", densidade1);
+printf("PIB per capita: %.2f\n", pibcapita1);
+
+printf("\nCarta 2: \n\n");
+printf("Estado: %c \n", estado2);
+printf("Código: %s \n", codigo2);
+printf("Cidade: %s \n", cidade2);
+printf("População: %d\n", populacao2);
+printf("Área: %.2f km²\n", area2);
+printf("PIB: %.2f\n", pib2);
+printf("Pontos turísticos: %d\n", nturisticos2);
+printf("Densidade populacional: %.2f\n", densidade2);
+printf("PIB per capita: %.2f\n", pibcapita2);
 return 0;
-} 
+
+
+}
